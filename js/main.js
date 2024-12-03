915946dc9805e5ce6509bc799e60a23a71f76f72
Maihetemy <maihetemy@gmail.com>
2024-12-03 04:08:39 +0200

    switch between signup and login

--- js/main.js
+++ js/main.js
@@ -4,13 +4,24 @@ var signUpName = document.getElementById('signUpName');
 var signUpEmail = document.getElementById('signUpEmail');
 var signUpPassword = document.getElementById('signUpPassword');
 var signUpButton = document.getElementById('signUpButton');
+var loginButton = document.getElementById('loginButton');
+
+var title = document.querySelector('h1');
+var signInAndUpSapn = document.getElementById('signInAndUpSapn');
 
 var usersList = [];
-// console.log(signUpName.value);
-// console.log(signUpEmail.value);
-// console.log(signUpPassword.value);
+if(localStorage.getItem('users')){
+    usersList = JSON.parse(localStorage.getItem('users'));
+}
 
-signUpButton.addEventListener('click', function () {addUser();});
+// sign up event
+signUpButton.addEventListener('click', function () {
+    addUser();
+    switchSpan();
+    switchButtons();
+    removeNameInput();
+    changeTitleName();
+});
 
 function addUser() {
     var user = {
@@ -22,3 +33,22 @@ function addUser() {
     localStorage.setItem('users', JSON.stringify(usersList));
     console.log(usersList);
 }
+
+// ---------------------------------
+
+
+// switch functions
+function switchSpan() {
+    signInAndUpSapn.innerHTML = 'sign up';
+}
+function switchButtons() {
+    loginButton.classList.replace('d-none', 'd-block');
+    signUpButton.classList.replace('d-block', 'd-none');
+}
+function removeNameInput(){
+    signUpName.classList.replace('d-block', 'd-none');
+}
+function changeTitleName(){
+    title.innerHTML = 'Smart login System';
+}
+
